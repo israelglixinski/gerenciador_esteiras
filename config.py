5d126bf7090bce71ea049dcd096da2f109bb52e0
Israel Glixinski <israel_glixinski@hotmail.com>
2023-11-04 17:03:27 -0300

    melhorado atualização

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 
-
-
-
 class Projeto_config():
-
+    'Cria o objeto que receberá as configurações do projeto'
     def __init__(self
     ,nome_projeto       
     ,endereco_repositorio    
@@ -13,39 +10,39 @@ class Projeto_config():
     ,branch_desenvolvimento  
     ,script_inicial          
     ) :
-        self.nome_projeto               = nome_projeto       
-        self.endereco_repositorio       = endereco_repositorio    
-        self.pasta_local_projeto        = pasta_local_projeto     
-        self.branch_producao            = branch_producao         
-        self.branch_homologacao         = branch_homologacao      
-        self.branch_desenvolvimento     = branch_desenvolvimento  
-        self.script_inicial             = script_inicial          
+        self.nome_projeto               = nome_projeto                          #* DE PREFERENCIA O MESMO NOME DO PROJETO NO REPOSITÓRIO  
+        self.endereco_repositorio       = endereco_repositorio                  #* URL DO ARQUIVO .GIT DO REPOSITÓRIO
+        self.pasta_local_projeto        = pasta_local_projeto                   #* PASTA LOCAL QUE RECEBERÁ A PASTA DO REPOSITÓRIO CLONADO
+        self.branch_producao            = branch_producao                       #* NOME DA BRANCH DE PRODUÇÃO
+        self.branch_homologacao         = branch_homologacao                    #* NOME DA BRANCH DE HOMOLOGAÇÃO
+        self.branch_desenvolvimento     = branch_desenvolvimento                #* NOME DA BRANCH DE DESENVOLVIMENTO
+        self.script_inicial             = script_inicial                        #* SCRIPT PYTHON QUE SERÁ INICIADO OU REINICIADO A CADA ATUALIZAÇÃO
 
 
 
-
-
-proj_IGPyAppFront               = Projeto_config(
-    nome_projeto                = 'IGPyAppFront'
-    ,endereco_repositorio       = 'https://github.com/israelglixinski/IGPyAppFront.git' 
-    ,pasta_local_projeto        = 'C:/Projetos'
-    ,branch_producao            = 'master'
-    ,branch_homologacao         = 'hom'
-    ,branch_desenvolvimento     = 'dev'
-    ,script_inicial             = 'main.py'
+proj_IGPyAppFront                       = Projeto_config(
+    nome_projeto                        = 'IGPyAppFront'
+    ,endereco_repositorio               = 'https://github.com/israelglixinski/IGPyAppFront.git' 
+    ,pasta_local_projeto                = 'C:/Projetos'
+    ,branch_producao                    = 'master'
+    ,branch_homologacao                 = 'hom'
+    ,branch_desenvolvimento             = 'dev'
+    ,script_inicial                     = 'main.py'
     )
 
-proj_bot_sms                    = Projeto_config(
-    nome_projeto                = 'bot_sms'
-    ,endereco_repositorio       = 'https://github.com/israelglixinski/bot_sms.git' 
-    ,pasta_local_projeto        = 'C:/Projetos'
-    ,branch_producao            = 'master'
-    ,branch_homologacao         = 'hom'
-    ,branch_desenvolvimento     = 'dev'
-    ,script_inicial             = 'main.py'
+proj_bot_sms                            = Projeto_config(
+    nome_projeto                        = 'bot_sms'
+    ,endereco_repositorio               = 'https://github.com/israelglixinski/bot_sms.git' 
+    ,pasta_local_projeto                = 'C:/Projetos'
+    ,branch_producao                    = 'master'
+    ,branch_homologacao                 = 'hom'
+    ,branch_desenvolvimento             = 'dev'
+    ,script_inicial                     = 'main.py'
     )
 
 
+
+##### * AQUI DEFINIMOS OS PROJETOS QUE DESEJAMOS QUE SEJAM MONITORADOS
 lista_projetos_executar = [proj_IGPyAppFront,proj_bot_sms]
 
 
--- executor.py
+++ executor.py
@@ -1,20 +1,21 @@
 import config
-import os
 import git
+import os
+from time import sleep
 
 
 for projeto in config.lista_projetos_executar:
     
-    ##### * PARA TESTE DAS VARIAVEIS DO PROJETO 
-    print('\n')
-    print(f'nome_projeto            = {projeto.nome_projeto             }')
-    print(f'endereco_repositorio    = {projeto.endereco_repositorio     }')
-    print(f'pasta_local_projeto     = {projeto.pasta_local_projeto      }')
-    print(f'branch_producao         = {projeto.branch_producao          }')
-    print(f'branch_homologacao      = {projeto.branch_homologacao       }')
-    print(f'branch_desenvolvimento  = {projeto.branch_desenvolvimento   }')
-    print(f'script_inicial          = {projeto.script_inicial           }')
-    print('\n')
+    # ##### * PARA TESTE DAS VARIAVEIS DO PROJETO 
+    # print('\n')
+    # print(f'nome_projeto            = {projeto.nome_projeto             }')
+    # print(f'endereco_repositorio    = {projeto.endereco_repositorio     }')
+    # print(f'pasta_local_projeto     = {projeto.pasta_local_projeto      }')
+    # print(f'branch_producao         = {projeto.branch_producao          }')
+    # print(f'branch_homologacao      = {projeto.branch_homologacao       }')
+    # print(f'branch_desenvolvimento  = {projeto.branch_desenvolvimento   }')
+    # print(f'script_inicial          = {projeto.script_inicial           }')
+    # print('\n')
 
     if os.path.exists(projeto.pasta_local_projeto): pass                                    #* VERIFICA SE EXISTE A PASTA QUE RECEBERÁ O PROJETO 
     else: os.makedirs(projeto.pasta_local_projeto)                                          #* CASO NÃO EXISTA, CRIA A PASTA
@@ -24,10 +25,21 @@ for projeto in config.lista_projetos_executar:
         ##### * CASO JÁ EXISTA A PASTA VERIFICAMOS SE EXISTE A PASTA GIT
         if os.path.exists(f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}/.git'): 
             ##### * CASO JÁ EXISTA A PASTA É SÓ REALIZAR O PULL
-            repo = git.Repo(f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}')
-            o = repo.remotes.origin
-            o.pull()
+            repositorio = git.Repo(f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}')
+            repositorio_remoto = repositorio.remotes.origin 
+
+            last_commit_local = repositorio.commit().committed_datetime
+            last_commit_remot = repositorio_remoto.fetch()[0].commit.committed_datetime
+        
+            if last_commit_local == last_commit_remot:
+                print(f"{projeto.nome_projeto} - Estamos com a versão mais recente")
+            else:
+                print(f"{projeto.nome_projeto} - Precisamos atualizar o repositório")
+                repositorio_remoto.pull()
+            sleep(3)
             pass
+            
+        
         else:
             ##### * MAS SE NÃO EXISTIR A PASTA DO GIT, ENTÃO INFORMAMOS O ERRO. POIS NÃO SERÁ POSSIVEL CLONAR EM UMA PASTA JÁ EXISTENTE
             print(f"ERRO - VERIFIQUE O PROJETO: {projeto.nome_projeto}")
@@ -35,5 +47,4 @@ for projeto in config.lista_projetos_executar:
         ##### * SE AINDA NÃO EXISTE A PASTA DO REPOSITÓRIO ENTÃO FAZEMOS O CLONE DO REPOSITÓRIO
         git.Repo.clone_from(projeto.endereco_repositorio, f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}')
         pass
-
 
