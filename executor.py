8537b2121fe23f4022c70d5ac54119ae95b1bb28
Israel Glixinski <israel_glixinski@hotmail.com>
2023-11-05 20:41:15 -0300

    grande melhoria no executor

--- executor.py
+++ executor.py
@@ -4,6 +4,7 @@ import git
 import os
 
 
+
 for projeto in config.lista_projetos_executar:
     
     ##### * PARA TESTE DAS VARIAVEIS DO PROJETO 
@@ -18,33 +19,29 @@ for projeto in config.lista_projetos_executar:
     print(f'lista_ambientes_ativos  = {projeto.lista_ambientes_ativos   }')
     print('\n')
 
-    if os.path.exists(projeto.pasta_local_projeto): pass        #* VERIFICA SE EXISTE A PASTA QUE RECEBERÁ O PROJETO 
-    else: os.makedirs(projeto.pasta_local_projeto)              #* CASO NÃO EXISTA, CRIA A PASTA
+    for branch_proj in projeto.lista_ambientes_ativos:
+        if branch_proj == 'PROD': my_branch = projeto.branch_producao
+        if branch_proj == 'HOM' : my_branch = projeto.branch_homologacao
+        if branch_proj == 'DEV' : my_branch = projeto.branch_desenvolvimento
 
-    ##### * VERIFICAMOS SE TEMOS A PASTA DO REPOSITÓRIO NA PASTA QUE RECEBERÁ O PROJETO
-    if os.path.exists(f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}'): 
-        ##### * CASO JÁ EXISTA A PASTA VERIFICAMOS SE EXISTE A PASTA GIT
-        if os.path.exists(f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}/.git'): 
-            ##### * CASO JÁ EXISTA A PASTA É SÓ REALIZAR O PULL
-            repositorio = git.Repo(f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}')
+        path_branch_repositorio = f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}/{my_branch}'
+        if os.path.exists(path_branch_repositorio):  pass
+        else: os.makedirs(path_branch_repositorio)
+
+        if os.path.exists(f'{path_branch_repositorio}/.git') == False: 
+            git.Repo.clone_from(projeto.endereco_repositorio, path_branch_repositorio,branch=my_branch)
+        else:
+            repositorio = git.Repo(path_branch_repositorio)
             repositorio_remoto = repositorio.remotes.origin 
 
             last_commit_local = repositorio.commit().committed_datetime
             last_commit_remot = repositorio_remoto.fetch()[0].commit.committed_datetime
         
             if last_commit_local == last_commit_remot:
-                print(f"{projeto.nome_projeto} - Estamos com a versão mais recente")
+                print(f"{projeto.nome_projeto} / {my_branch} - Estamos com a versão mais recente")
             else:
-                print(f"{projeto.nome_projeto} - Precisamos atualizar o repositório")
+                print(f"{projeto.nome_projeto} / {my_branch} - Precisamos atualizar o repositório")
                 repositorio_remoto.pull()
             sleep(3)
-            pass
-        
-        else:
-            ##### * MAS SE NÃO EXISTIR A PASTA DO GIT, ENTÃO INFORMAMOS O ERRO. POIS NÃO SERÁ POSSIVEL CLONAR EM UMA PASTA JÁ EXISTENTE
-            print(f"ERRO - VERIFIQUE O PROJETO: {projeto.nome_projeto}")
-    else:
-        ##### * SE AINDA NÃO EXISTE A PASTA DO REPOSITÓRIO ENTÃO FAZEMOS O CLONE DO REPOSITÓRIO
-        git.Repo.clone_from(projeto.endereco_repositorio, f'{projeto.pasta_local_projeto}/{projeto.nome_projeto}')
-        pass
+
 
