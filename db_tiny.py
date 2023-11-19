4318a28bc11487e3f752f704b41c05dc8f4807c0
Israel Glixinski <israel_glixinski@hotmail.com>
2023-11-19 15:10:07 -0300

    alterado de mult-branchs, para apenas uma

--- db_tiny.py
+++ db_tiny.py
@@ -12,10 +12,9 @@ configs             = db_tiny.table("configs")
 def insert_projetos(
  nome_proj
 ,url_rep
-,destino_local
 ,script_inicial
 ,script_inicial_pid
-,branchs
+,branch
 ,monitoramento
 ,monitoramento_param
 ,monitoramento_valores
@@ -24,10 +23,9 @@ def insert_projetos(
         {
          "nome_proj"                : nome_proj        
         ,"url_rep"                  : url_rep          
-        ,"destino_local"            : destino_local    
         ,"script_inicial"           : script_inicial   
         ,"script_inicial_pid"       : script_inicial_pid   
-        ,"branchs"                  : branchs          
+        ,"branch"                   : branch          
         ,"monitoramento"            : monitoramento
         ,"monitoramento_param"      : monitoramento_param
         ,"monitoramento_valores"    : monitoramento_valores
@@ -35,8 +33,9 @@ def insert_projetos(
     )
     pass
 
-def insert_configs(tempo_loop_minutos):
-    configs.insert({"tempo_loop_minutos":tempo_loop_minutos})
+def insert_configs(tempo_loop_minutos,destino_local):
+    configs.insert({"tempo_loop_minutos"    :tempo_loop_minutos })
+    configs.insert({"destino_local"         :destino_local      })
     pass
 
 def update_pid(nome_proj,pid):
@@ -45,55 +44,65 @@ def update_pid(nome_proj,pid):
     pass
 
 def popula_inicial():
-    insert_configs(tempo_loop_minutos=1)
+    
+    insert_configs(
+         tempo_loop_minutos = 1
+        ,destino_local      = "c:/Projetos"
+        )
 
     insert_projetos(
-     nome_proj              = "exemplo_robo"
-    ,url_rep                = "https://github.com/israelglixinski/exemplo_robo.git"
-    ,destino_local          = "C:/Projetos"
-    ,script_inicial         = "main.py"
-    ,script_inicial_pid     = 0
-    ,branchs                = ["main"]
-    ,monitoramento          = "log"
-    ,monitoramento_param    =   {"path_log_file"        : "./logs/log.txt"
-                                ,"minutos_max_sem_alt"  : 5
-                                }
-    ,monitoramento_valores  =   {"ultima_alteracao"     : None}
-    )
+        nome_proj              = "exemplo_robo"
+        ,url_rep                = "https://github.com/israelglixinski/exemplo_robo.git"
+        ,script_inicial         = "main.py"
+        ,script_inicial_pid     = 0
+        ,branch                 = "main"
+        ,monitoramento          = "log"
+        ,monitoramento_param    =   {"path_log_file"        : "./logs/log.txt"
+                                    ,"minutos_max_sem_alt"  : 5
+                                    }
+        ,monitoramento_valores  =   {"ultima_alteracao"     : None}
+        )
 
     insert_projetos(
-     nome_proj              = "exemplo_web"
-    ,url_rep                = "https://github.com/israelglixinski/exemplo_web.git"
-    ,destino_local          = "C:/Projetos"
-    ,script_inicial         = "inicial.py"
-    ,script_inicial_pid     = 0
-    ,branchs                = ["main"]
-    ,monitoramento          = "web"
-    ,monitoramento_param    =   {"url"                           : "localhost"
-                                ,"max_tentativas"                :5
-                                ,"segundos_max_cada_tentativa"   :60
-                                }
-    ,monitoramento_valores  =   {"ultima_confirmacao":None}
-    )
+        nome_proj              = "exemplo_web"
+        ,url_rep                = "https://github.com/israelglixinski/exemplo_web.git"
+        ,script_inicial         = "inicial.py"
+        ,script_inicial_pid     = 0
+        ,branch                 = "main"
+        ,monitoramento          = "web"
+        ,monitoramento_param    =   {"url"                           : "localhost"
+                                    ,"max_tentativas"                :5
+                                    ,"segundos_max_cada_tentativa"   :60
+                                    }
+        ,monitoramento_valores  =   {"ultima_confirmacao":None}
+        )
     pass
 
 def recupera_configs():
     class Obj_configs:
         def __init__(self):
-            self.tempo_loop_minutos = None
+            self.tempo_loop_minutos     = None
+            self.destino_local          = None
             pass
     obj_configs = Obj_configs()
 
     querry_configs = Query()
-    obj_configs.tempo_loop_minutos = configs.search(querry_configs["tempo_loop_minutos"].exists())[-1]["tempo_loop_minutos"]
+    obj_configs.tempo_loop_minutos  = configs.search(querry_configs["tempo_loop_minutos"    ].exists())[-1]["tempo_loop_minutos"    ]
+    obj_configs.destino_local       = configs.search(querry_configs["destino_local"         ].exists())[-1]["destino_local"         ]
     
     return obj_configs
+
+def recupera_projeto(nome_proj):
+    querry_projeto = Query()
+    consulta = projetos.search(querry_projeto["nome_proj"] == nome_proj)[0]
+    return consulta
 
 
 if __name__ == '__main__':
     # configs.insert({"teste":"primeiro"})
-    recupera_configs()
-
+    # popula_inicial()
+    # recupera_configs()
+    # recupera_projeto('exemplo_robo')
 
 
     pass
--- sincronizador.py
+++ sincronizador.py
@@ -1,67 +1,89 @@
+from datetime import datetime
 from subprocess import Popen
-import db_tiny
 from time import sleep
+import db_tiny
 import git
 import os
-from datetime import datetime
 
 
+##### * Carregamento inicial das informações do banco
+projetos    = db_tiny.projetos
+configs     = db_tiny.recupera_configs()
 
-projetos = db_tiny.projetos
+def registrando(texto):
+    '''Para registro, exibição e monitoração das ações'''
+    print(f'{datetime.now().strftime("%Y/%m/%d %H:%M:%S")} - {texto}')
 
-##### * definir todos os projetos como não inicializados
-##### * fazer setando os pids como zero
+def iniciar_servico_projeto(nome_projeto):
+    projeto = db_tiny.recupera_projeto(nome_projeto)
+    path_script_inicial = f'{configs.destino_local}/{projeto["nome_proj"]}/{projeto["branch"]}/{projeto["script_inicial"]}'
+    start_processo = Popen(f'python {path_script_inicial}')   
+    db_tiny.update_pid(projeto['nome_proj'],start_processo.pid)
+    pass
 
 
+##### * Define todos os projetos como não inicializados, setando o PID como zero
+for projeto in projetos:
+    db_tiny.update_pid(projeto['nome_proj'],0)
 
-
-
-def executar():
+def rotina():
+    '''Ações principais para sincronização e execução dos projetos'''
+    
+    ##### * Para cada projeto na lista de projetos
     for projeto in projetos:
-        for branch in projeto["branchs"]:
-            path_branch_repositorio = f'{projeto["destino_local"]}/{projeto["nome_proj"]}/{branch}'
-            if os.path.exists(path_branch_repositorio):  pass
-            else: os.makedirs(path_branch_repositorio)
+        registrando(f"Trabalhando com o projeto: '{projeto['nome_proj']}'")
+        
+        ##### * Caminho em que será trabalhado no projeto
+        path_branch_repositorio = f'{configs.destino_local}/{projeto["nome_proj"]}/{projeto["branch"]}'
+        
+        ##### * Verifica se a pasta de destino do projeto já existe
+        if os.path.exists(path_branch_repositorio):  pass                                   #* Caso a pasta já exista não é necessário mais ações
+        else:                                                                               #* Mas caso não exista é necessário criala
+            registrando('Não foi encontrado a pasta de destino do projeto')
+            os.makedirs(path_branch_repositorio)
+            registrando('Criado uma nova pasta para trabalhar no projeto')
 
-            if os.path.exists(f'{path_branch_repositorio}/.git') == False: 
-                git.Repo.clone_from(projeto["url_rep"], path_branch_repositorio,branch=branch)
-                start_processo = Popen(f'python {path_branch_repositorio}/{projeto["script_inicial"]}')           
-                db_tiny.update_pid(projeto['nome_proj'],start_processo.pid)
+        ##### * Verificação se já temos o repositório clonado na pasta de destino
+        if os.path.exists(f'{path_branch_repositorio}/.git') == False:                      #* Caso ainda não esteja clonado, énecessário clonar           
+            registrando('Não foi encontrado o repositório, iniciando clone...') 
+            git.Repo.clone_from(projeto["url_rep"], path_branch_repositorio,branch=projeto["branch"])
+            registrando('Finalizado clone') 
+            iniciar_servico_projeto(projeto["nome_proj"])                                   #* Inicia o script inicial do projeto
 
+
+        else:
+            repositorio = git.Repo(path_branch_repositorio)
+            repositorio_remoto = repositorio.remotes.origin 
+
+            last_commit_local = repositorio.commit().committed_datetime
+            last_commit_remot = repositorio_remoto.fetch()[0].commit.committed_datetime
+        
+            if last_commit_local == last_commit_remot:
+                registrando(f'{projeto["nome_proj"]} / {projeto["branch"]} - Estamos com a versão mais recente')
 
             else:
-                repositorio = git.Repo(path_branch_repositorio)
-                repositorio_remoto = repositorio.remotes.origin 
-
-                last_commit_local = repositorio.commit().committed_datetime
-                last_commit_remot = repositorio_remoto.fetch()[0].commit.committed_datetime
-            
-                if last_commit_local == last_commit_remot:
-                    print(f"{projeto['nome_proj']} / {branch} - Estamos com a versão mais recente")
-
-                else:
-                    print(f"{projeto['nome_proj']} / {branch} - Precisamos atualizar o repositório")
-                    repositorio_remoto.pull()
-                sleep(3)
+                registrando(f'{projeto["nome_proj"]} / {projeto["branch"]} - Precisamos atualizar o repositório')
+                repositorio_remoto.pull()
+            sleep(3)
 
 def iniciar_projetos():
     for projeto in projetos:
-        for branch in projeto["branchs"]:
-            path_branch_repositorio = f'{projeto["destino_local"]}/{projeto["nome_proj"]}/{branch}'
-            
-            start_processo = Popen(f'python {path_branch_repositorio}/{projeto["script_inicial"]}')           
-            print (f'{projeto["script_inicial"]} - {start_processo.pid}')
-            db_tiny.update_pid(projeto['nome_proj'],start_processo.pid)
+        path_branch_repositorio = f'{configs.destino_local}/{projeto["nome_proj"]}/{projeto["branch"]}'
+        
+        start_processo = Popen(f'python {path_branch_repositorio}/{projeto["script_inicial"]}')           
+        print (f'{projeto["script_inicial"]} - {start_processo.pid}')
+        db_tiny.update_pid(projeto['nome_proj'],start_processo.pid)
             
 
-            pass
+        pass
     pass
 
 
 def orquestrador():
     while True:
-        # executar()
-        print(datetime.now())
+        registrando('Iniciando rotina')
+        rotina()
+        registrando('Finalizado rotina')
         configs = db_tiny.recupera_configs()
         sleep(60*configs.tempo_loop_minutos)
         pass
